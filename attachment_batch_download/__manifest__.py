f211a42df5975aeba4ff5e8aca3378cc8b5a6a69
Aungkokolin1997 <aungkokolin23397@gmail.com>
2022-11-14 17:07:25 +0630

    [2941][ADD] download_account_move_multiple_attachments

---
+++ attachment_batch_download/__manifest__.py
@@ -0,0 +1,15 @@
+# Copyright 2022 Quartile Limited
+{
+    "name": "Attachment Batch Download",
+    "category": "Attachment",
+    "version": "14.0.1.0.0",
+    "author": "Quartile Limited",
+    "website": "https://www.quartile.co",
+    "license": "AGPL-3",
+    "depends": ["mail", "base"],
+    "data": [
+        "security/ir.model.access.csv",
+        "views/generate_model_download.xml",
+    ],
+    "installable": True,
+}
