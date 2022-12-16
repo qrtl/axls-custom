b77e25bb9824e55d9fe668b26a6849d1563f082b
Aungkokolin1997 <aungkokolin23397@gmail.com>
2022-12-16 12:52:23 +0630

    [ADD] currency_rate_update_mizuho

---
+++ currency_rate_update_mizuho/__manifest__.py
@@ -0,0 +1,12 @@
+# Copyright 2022 Quartile Limited
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+{
+    "name": "Currency Rate Update Mizuho",
+    "version": "16.0.1.0.0",
+    "category": "Financial Management/Configuration",
+    "author": "Axelspace, Quartile Limited",
+    "website": "https://www.quartile.co",
+    "depends": ["currency_rate_update"],
+    "license": "AGPL-3",
+    "installable": True,
+}
