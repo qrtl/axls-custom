76bef8c314bf1b6ec8bb81f71ed4f158809f51f9
Yoshi Tashiro <tashiro@quartile.co>
2022-12-30 03:57:06 +0000

    [2890][IMP] product_alternative_code: add product views

--- product_alternative_code/models/product_template.py
+++ product_alternative_code/models/product_template.py
@@ -8,3 +8,22 @@ class ProductTemplate(models.Model):
     _inherit = "product.template"
 
     alt_code = fields.Char("Alternative Code", help="Alternative product code.")
+
+    def name_get(self):
+        res = super().name_get()
+        name_list = []
+        for rec in res:
+            product = self.browse(rec[0])
+            alt_code = product.alt_code
+            if not alt_code:
+                name_list.append(rec)
+                continue
+            name = rec[1]
+            if not product.default_code:
+                name = "[" + alt_code + "] " + name
+                name_list.append((rec[0], name))
+                continue
+            pos = name.find("]")
+            name = name[:pos] + "/" + alt_code + name[pos:]
+            name_list.append((rec[0], name))
+        return name_list
