fce11e307bc20a2ee5f64d0f0d551bb920677396
Yoshi Tashiro (Quartile) <tashiro@quartile.co>
2022-12-30 13:28:37 +0900

    [2890][IMP] product_alternative_code: add product views (#12)

--- product_alternative_code/__manifest__.py
+++ product_alternative_code/__manifest__.py
@@ -2,13 +2,14 @@
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 {
     "name": "Product Alternative Code",
-    "version": "16.0.1.0.0",
+    "version": "16.0.1.1.0",
     "author": "Quartile Limited",
     "website": "https://www.quartile.co",
     "category": "Product",
     "license": "LGPL-3",
     "depends": ["product"],
     "data": [
+        "views/product_product_views.xml",
         "views/product_template_views.xml",
     ],
     "installable": True,
