6cf08ffe311d6ef45b61c20055c65bab23b2520f
Aungkokolin1997 <lin@quartile.co>
2023-05-17 14:19:47 +0630

    [ADD] stock_move_line_move_location

---
+++ stock_move_line_move_location/__manifest__.py
@@ -0,0 +1,17 @@
+# Copyright 2023 Quartile Limited
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
+
+{
+    "name": "Move Stock Line Move Location",
+    "version": "16.0.1.0.0",
+    "author": "Quartile Limited",
+    "summary": "This module allows to move all stock "
+    "in a stock location to an other one in moves history.",
+    "website": "https://www.quartile.co",
+    "license": "AGPL-3",
+    "depends": ["stock_move_location"],
+    "category": "Stock",
+    "data": [
+        "data/stock_move_line_view.xml",
+    ],
+}
---
+++ stock_move_line_move_location/readme/DESCRIPTION.rst
@@ -0,0 +1,2 @@
+This module allows to move location of products from one place to
+another in move history(stock move line).
