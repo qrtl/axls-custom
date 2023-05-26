ea6c0a36c12f0524f44e0602bfdcc34ec14c27a4
Yoshi Tashiro <tashiro@quartile.co>
2023-05-26 14:37:24 +0000

    fixes and adjustments

--- stock_move_line_move_location/readme/DESCRIPTION.rst
+++ stock_move_line_move_location/readme/DESCRIPTION.rst
@@ -1,2 +1,6 @@
-This module allows to move location of products from one place to
-another in move history(stock move line).
+This module extends the function of stock_move_location module to allow creation of
+transfers based on stock.move.line records.
+
+The module was developed for a rather peculiar need of just removing the inventory
+of the received products, which has been consumed in production straight away, where
+the user of the manufacturing order has not been implemented.
--- stock_move_line_move_location/wizard/stock_move_location.py
+++ stock_move_line_move_location/wizard/stock_move_location.py
@@ -14,64 +14,49 @@ class StockMoveLocationWizard(models.TransientModel):
     @api.model
     def default_get(self, fields):
         res = super().default_get(fields)
-        if self.env.context.get("active_model", False) == "stock.move.line":
-            # Load data from move lines
-            move_lines = self.env["stock.move.line"].browse(
-                self.env.context.get("active_ids", False)
-            )
-            if len(move_lines.location_dest_id.ids) > 1:
-                raise UserError(
-                    _("There is more than one location in selected move lines.")
-                )
-            res["stock_move_location_line_ids"] = self.with_context(
-                move_line_move_location=True
-            )._prepare_wizard_move_lines(move_lines)
-            res["origin_location_id"] = first(move_lines).location_dest_id.id
+        if self.env.context.get("active_model", False) != "stock.move.line":
             return res
+        # Load data from move lines
+        move_lines = self.env["stock.move.line"].browse(
+            self.env.context.get("active_ids", False)
+        )
+        if len(move_lines.location_dest_id) > 1:
+            raise UserError(
+                _("There is more than one location in selected move lines.")
+            )
+        res["stock_move_location_line_ids"] = self.with_context(
+            move_line_move_location=True
+        )._prepare_wizard_move_lines(move_lines)
+        res["origin_location_id"] = first(move_lines).location_dest_id.id
         return res
 
     @api.model
     def _prepare_wizard_move_lines(self, move_lines):
         res = []
-        move_line_move_location = self.env.context.get("move_line_move_location", False)
-        if move_line_move_location:
-            # if need move only available qty per product on location
-            for _product, ml in groupby(
-                sorted(move_lines, key=lambda r: (r.product_id.id, r.lot_id.id)),
-                lambda r: (r.product_id.id, r.lot_id.id),
-            ):
-                quant = self.env["stock.quant"]
-                ml = list(ml)[0]
-                qty = quant._get_available_quantity(
-                    ml.product_id,
-                    ml.location_dest_id,
-                    ml.lot_id,
-                    strict=True,
-                )
-                # _get_available_quantity return total qty of lot and no lot quants
-                # Deduct no lot qty
-                if ml.lot_id:
-                    qty_without_lot = quant._get_available_quantity(
-                        ml.product_id, ml.location_dest_id, lot_id=False, strict=True
-                    )
-                    qty -= qty_without_lot
-                if qty:
-                    res.append(
-                        (
-                            0,
-                            0,
-                            {
-                                "product_id": ml.product_id.id,
-                                "move_quantity": qty,
-                                "max_quantity": qty,
-                                "origin_location_id": ml.location_dest_id.id,
-                                "lot_id": ml.lot_id.id,
-                                "product_uom_id": ml.product_uom_id.id,
-                                "custom": False,
-                            },
-                        )
-                    )
-            return res
-        return super(StockMoveLocationWizard, self)._prepare_wizard_move_lines(
-            move_lines
-        )
+        if not self.env.context.get("move_line_move_location", False):
+            return super()._prepare_wizard_move_lines(move_lines)
+        # if need move only available qty per product on location
+        for _product, ml in groupby(
+            sorted(move_lines, key=lambda r: (r.product_id, r.lot_id)),
+            lambda r: (r.product_id, r.lot_id),
+        ):
+            quant = self.env["stock.quant"]
+            ml = list(ml)[0]
+            qty = quant._get_available_quantity(
+                ml.product_id,
+                ml.location_dest_id,
+                ml.lot_id,
+                strict=True,
+            )
+            if qty:
+                vals = {
+                    "product_id": ml.product_id.id,
+                    "move_quantity": qty,
+                    "max_quantity": qty,
+                    "origin_location_id": ml.location_dest_id.id,
+                    "lot_id": ml.lot_id.id,
+                    "product_uom_id": ml.product_id.uom_id.id,
+                    "custom": False,
+                }
+                res.append((0, 0, vals))
+        return res
