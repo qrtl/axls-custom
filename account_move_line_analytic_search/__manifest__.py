3cba1623ba6ee3f0c229b568845b51bda6b7d636
Rinaldi Firdaus <rinaldi@quartile.co>
2023-05-29 06:49:40 +0000

    [3326][ADD] account_move_line_analytic_search

---
+++ account_move_line_analytic_search/__manifest__.py
@@ -0,0 +1,15 @@
+# Copyright 2023 Quartile Limited
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Account Move Line Analytic Search",
+    "version": "16.0.1.0.0",
+    "author": "Quartile Limited",
+    "website": "https://www.quartile.co",
+    "category": "Accounting",
+    "license": "AGPL-3",
+    "depends": ["account"],
+    "data": [
+        "views/account_move_views.xml",
+    ],
+    "installable": True,
+}
---
+++ account_move_line_analytic_search/readme/DESCRIPTION.rst
@@ -0,0 +1,1 @@
+This module adds analytic_distribution on Journal Items's search view
