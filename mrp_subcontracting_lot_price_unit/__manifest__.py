cad8437363b891b48e31767e85f7fbeb542e1fce
Yoshi Tashiro <tashiro@quartile.co>
2023-06-17 15:21:20 +0000

    [3422][ADD] stock_lot_price_unit

---
+++ mrp_subcontracting_lot_price_unit/__manifest__.py
@@ -0,0 +1,13 @@
+# Copyright 2023 Quartile Limited (https://www.quartile.co)
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+{
+    "name": "MRP Subcontracting Lot Price Unit",
+    "version": "16.0.1.0.0",
+    "author": "Quartile Limited",
+    "website": "https://www.quartile.co",
+    "category": "Warehouse Management",
+    "license": "AGPL-3",
+    "depends": ["mrp_subcontracting", "stock_lot_price_unit"],
+    "installable": True,
+    "auto_install": True,
+}
---
+++ stock_lot_price_unit/__manifest__.py
@@ -0,0 +1,16 @@
+# Copyright 2023 Quartile Limited (https://www.quartile.co)
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Stock Lot Price Unit",
+    "version": "16.0.1.0.0",
+    "author": "Quartile Limited",
+    "website": "https://www.quartile.co",
+    "category": "Warehouse Management",
+    "license": "AGPL-3",
+    "depends": ["stock"],
+    "data": [
+        "views/stock_lot_views.xml",
+        "views/stock_quant_views.xml",
+    ],
+    "installable": True,
+}
