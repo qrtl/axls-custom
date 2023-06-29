165f84e5fa229527b9fb01fbabe741f1e781a15f
Aungkokolin1997 <lin@quartile.co>
2023-06-29 08:20:11 +0630

    [FIX] stock_lot_analytic: restrict inventory adjustments

--- stock_lot_analytic/models/stock_quant.py
+++ stock_lot_analytic/models/stock_quant.py
@@ -1,7 +1,7 @@
 # Copyright 2023 Quartile Limited
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from odoo import fields, models
+from odoo import api, fields, models
 
 
 class StockQuant(models.Model):
@@ -9,3 +9,9 @@ class StockQuant(models.Model):
     _inherit = ["stock.quant", "analytic.mixin"]
 
     analytic_distribution = fields.Json(related="lot_id.analytic_distribution")
+
+    @api.model
+    def _get_inventory_fields_create(self):
+        fields = super()._get_inventory_fields_create()
+        fields += ["analytic_distribution", "analytic_precision"]
+        return fields
