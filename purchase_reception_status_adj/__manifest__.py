e459fec07fba5223eb7f2f3da7faf9fae0baeea4
kanda999 <kanda@quartile.co>
2023-07-03 09:43:26 +0000

    [ADD] purchase_reception_status_adj

---
+++ purchase_reception_status_adj/__manifest__.py
@@ -0,0 +1,13 @@
+# Copyright 2023 Quartile Limited
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Purchase Reception Status Adjust",
+    "version": "16.0.1.0.0",
+    "author": "Quartile Limited",
+    "website": "https://www.quartile.co",
+    "category": "Purchase",
+    "license": "AGPL-3",
+    "depends": ["purchase","purchase_reception_status"],
+    "data": ["views/purchase_order.xml"],
+    "installable": True,
+}
