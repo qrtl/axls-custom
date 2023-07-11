73b14fa13e230f56d24fc2227340af0ecf14bc2b
Rinaldi Firdaus <rinaldi@quartile.co>
2023-07-11 05:27:16 +0000

    rename module and readme

--- purchase_order_line_description_optional/readme/DESCRIPTION.rst
+++
@@ -1,1 +0,0 @@
-This module adds an optional attribute to "name" field on purchase order line.
--- purchase_view_adj/__manifest__.py
+++ purchase_view_adj/__manifest__.py
@@ -1,7 +1,7 @@
 # Copyright 2023 Quartile Limited
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 {
-    "name": "Purchase Order Line Description Optional",
+    "name": "Purchase View Adj",
     "version": "16.0.1.0.0",
     "author": "Quartile Limited",
     "website": "https://www.quartile.co",
