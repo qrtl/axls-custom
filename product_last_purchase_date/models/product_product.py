90f108efcaf5febfec04919ab9b1aaeec61e3a1c
kanda999 <kanda@quartile.co>
2023-07-12 09:08:24 +0000

    Add _get_domain method

--- product_last_purchase_date/models/product_product.py
+++ product_last_purchase_date/models/product_product.py
@@ -20,19 +20,16 @@ class Product(models.Model):
         help="Date of the last receipt from the supplier.",
     )
 
-    def _assign_last_purchase_date(self, move):
-        return fields.Date.context_today(self, move.date)
-
     @api.depends("stock_move_ids.state", "man_last_purchase_date")
     def _compute_last_purchase_date(self):
         for product in self:
             last_purchase_date = False
             man_last_purchase_date = product.man_last_purchase_date
-            move = product.stock_move_ids.filtered(
-                lambda m: m.state == "done" and m.picking_code == "incoming"
-            ).sorted(key=lambda m: m.id, reverse=True)[:1]
+            move = self.env["stock.move"].search(
+                self._get_domain(product), order="id desc", limit=1
+            )
             if move:
-                last_purchase_date = self._assign_last_purchase_date(move)
+                last_purchase_date = fields.Date.context_today(self, move.date)
             if (
                 not last_purchase_date
                 or man_last_purchase_date
@@ -41,3 +38,10 @@ class Product(models.Model):
                 last_purchase_date = man_last_purchase_date
             product.last_purchase_date = last_purchase_date
             product.product_tmpl_id._compute_last_purchase_date()
+
+    def _get_domain(self, product):
+        return [
+            ("product_id", "=", product.id),
+            ("state", "=", "done"),
+            ("picking_code", "=", "incoming"),
+        ]
--- stock_move_ignore_last_purchase_date/models/product_product.py
+++ stock_move_ignore_last_purchase_date/models/product_product.py
@@ -1,28 +1,14 @@
 # Copyright 2023 Quartile Limited
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import fields, models
+from odoo import models
+from odoo.osv import expression
 
 
 class ProductProduct(models.Model):
     _inherit = "product.product"
 
-    def _assign_last_purchase_date(self, move):
-        if move.ignore_last_purchase_date:
-            new_move = self.stock_move_ids.filtered(
-                lambda m: m.state == "done"
-                and m.picking_code == "incoming"
-                and not m.ignore_last_purchase_date
-            ).sorted(key=lambda m: m.id, reverse=True)[:1]
-            if new_move:
-                move_date = fields.Date.context_today(self, new_move.date)
-                if self.man_last_purchase_date:
-                    if move_date > self.man_last_purchase_date:
-                        return move_date
-                    else:
-                        return self.man_last_purchase_date
-                else:
-                    return move_date
-            else:
-                return False
-        return super()._assign_last_purchase_date(move)
+    def _get_domain(self, product):
+        domain = super()._get_domain(product)
+        domain = expression.AND([domain, [("ignore_last_purchase_date", "=", False)]])
+        return domain
