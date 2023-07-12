a0f6a9659ea41193c4df29f604541fab82d9daf9
Aungkokolin1997 <lin@quartile.co>
2023-07-12 13:21:03 +0630

    upd

--- stock_move_ignore_last_purchase_date/models/product_product.py
+++ stock_move_ignore_last_purchase_date/models/product_product.py
@@ -16,10 +16,13 @@ class ProductProduct(models.Model):
             ).sorted(key=lambda m: m.id, reverse=True)[:1]
             if new_move:
                 move_date = fields.Date.context_today(self, new_move.date)
-                if move_date > self.man_last_purchase_date:
-                    return move_date
+                if self.man_last_purchase_date:
+                    if move_date > self.man_last_purchase_date:
+                        return move_date
+                    else:
+                        return self.man_last_purchase_date
                 else:
-                    return self.man_last_purchase_date
+                    return False
             else:
                 return False
         return super()._assign_last_purchase_date(move)
