a535dcf86523899dc1a612002eb92f3dd412f1e8
Rinaldi Firdaus <rinaldi@quartile.co>
2023-07-17 19:14:58 +0000

    [3494][ADD] purchase_order_qweb_report_adj

---
+++ purchase_order_qweb_report_adj/__manifest__.py
@@ -0,0 +1,15 @@
+# Copyright 2023 Quartile Limited
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Purchase Order Qweb Report Adj",
+    "version": "16.0.1.0.0",
+    "category": "",
+    "author": "Quartile Limited",
+    "website": "https://www.quartile.co",
+    "license": "AGPL-3",
+    "depends": ["base", "purchase"],
+    "data": [
+        "reports/purchase_order_templates.xml",
+    ],
+    "installable": True,
+}
