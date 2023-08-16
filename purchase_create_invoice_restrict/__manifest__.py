71c5a52d1d58fc0e339c1941251ae79416d502c0
kanda999 <kanda@quartile.co>
2023-08-16 07:28:10 +0000

    [3665][ADD] purchase_create_invoice_restrict

---
+++ purchase_create_invoice_restrict/__manifest__.py
@@ -0,0 +1,13 @@
+# Copyright 2023 Quartile Limited
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Purchase Create Invoice Restrict",
+    "version": "16.0.1.0.0",
+    "author": "Quartile Limited",
+    "website": "https://www.quartile.co",
+    "category": "Purchase",
+    "license": "AGPL-3",
+    "depends": ["account", "purchase"],
+    "data": ["views/purchase_order_views.xml", "security/purchase_security.xml"],
+    "installable": True,
+}
---
+++ purchase_create_invoice_restrict/readme/DESCRIPTION.rst
@@ -0,0 +1,1 @@
+Users with the group_account_invoice or group_create_invoice_allowed can press the "CREATE BILL" button.
