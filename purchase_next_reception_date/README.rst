109972f2922b1baf1593a280cb40cc330eaec2ec
Rinaldi Firdaus <rinaldi@quartile.co>
2023-08-24 10:12:11 +0000

    updates to handle service products next arrival date, add translation, update readme

--- purchase_next_reception_date/README.rst
+++ purchase_next_reception_date/README.rst
@@ -19,7 +19,16 @@ Purchase Next Reception Date
 
 |badge1| |badge2| |badge3| 
 
-This module adds next_reception_date in purchase order to recognize which is the date for next receipt.
+This module adds next_reception_date in purchase order to recognize the upcoming receipt date of the orders.
+
+Background
+~~~~~~~~~~
+
+For purchase orders with multiple order lines, each having different 'date_planned' values,
+expected arrival will not showing the upcoming receipt date after the first reception has been done.
+with this condition purchase users should open each record one by one to see the upcoming receipt of the orders.
+
+This Next Reception Date field is designed to give the information of the order's upcoming reception date at a glance on the purchase order list view.
 
 **Table of contents**
 
--- purchase_next_reception_date/models/purchase_order.py
+++ purchase_next_reception_date/models/purchase_order.py
@@ -12,12 +12,18 @@ class PurchaseOrder(models.Model):
     )
 
     @api.depends(
-        "order_line.date_planned", "order_line.product_qty", "order_line.qty_received"
+        "order_line.date_planned",
+        "order_line.product_qty",
+        "order_line.qty_received",
+        "order_line.qty_invoiced",
     )
     def _compute_next_reception_date(self):
         for order in self:
             dates = order.order_line.filtered(
-                lambda line: line.product_qty != line.qty_received
+                lambda line: line.product_id.detailed_type != "service"
+                and line.qty_received < line.product_qty
+                or line.product_id.detailed_type == "service"
+                and line.qty_invoiced < line.product_qty
             ).mapped("date_planned")
 
             order.next_reception_date = min(dates) if dates else False
