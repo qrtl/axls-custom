2026829de47d5cf6e371e9786ece0ac7d461b161
Aungkokolin1997 <lin@quartile.co>
2023-12-08 09:04:57 +0630

    [ADD] project_account_manager

---
+++ project_account_manger/__manifest__.py
@@ -0,0 +1,14 @@
+# Copyright 2023 Quartile Limited
+# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl).
+{
+    "name": "Project  Account Manager",
+    "version": "16.0.1.0.0",
+    "category": "project",
+    "website": "https://www.quartile.co",
+    "author": "Quartile Limited",
+    "license": "LGPL-3",
+    "depends": ["project"],
+    "data": [
+        "views/project_project_views.xml",
+    ],
+}
---
+++ project_account_manger/readme/DESCRIPTION.md
@@ -0,0 +1,1 @@
+This module adds the account_manager_ids field to the project.
