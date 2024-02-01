9ca409a302beebca43632a3144fb0a92ac3a44f5
Aungkokolin1997 <lin@quartile.co>
2024-02-01 11:41:56 +0630

    [ADD] purchase_security_adjust

---
+++ purchase_security_adjust/__manifest__.py
@@ -0,0 +1,13 @@
+# Copyright 2024 Quartile Limited
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Purchase Security Adjust",
+    "version": "16.0.1.0.0",
+    "author": "Quartile Limited",
+    "website": "https://www.quartile.co",
+    "category": "Purchase",
+    "license": "AGPL-3",
+    "depends": ["purchase"],
+    "data": ["security/purchase_security.xml", "views/purchase_order_views.xml"],
+    "installable": True,
+}
