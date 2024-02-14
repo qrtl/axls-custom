ce6aae31a0849648d3726ebec49080507d26ebd7
Aungkokolin1997 <lin@quartile.co>
2024-02-14 04:32:42 +0000

    [ADD] auth_oauth_block_password_login

---
+++ auth_oauth_force_oauth/models/res_company.py
@@ -0,0 +1,10 @@
+# Copyright 2024 Quartile Limited (https://www.quartile.co)
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+from odoo import fields, models
+
+
+class ResCompany(models.Model):
+    _inherit = "res.company"
+
+    force_oauth_domains = fields.Text(help="Fill in the domains, separated by commas.")
---
+++ auth_oauth_force_oauth/readme/DESCRIPTION.md
@@ -0,0 +1,3 @@
+This module controls login methods by email domain, requiring OAuth for
+specified domains, as defined in company
+settings.
