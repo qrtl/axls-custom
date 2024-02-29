237d7aac8badd5abf759e8a34f414a8f93489804
Yoshi Tashiro <tashiro@quartile.co>
2024-02-29 09:30:06 +0000

    [FIX] mrp_production_product_category: exclude subcontracting MOs

--- mrp_production_product_category_restriction/__manifest__.py
+++ mrp_production_product_category_restriction/__manifest__.py
@@ -1,13 +1,13 @@
 # Copyright 2024 Quartile Limited
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 {
-    "name": "Mrp Production Product Category Restriction",
-    "version": "16.0.1.0.0",
+    "name": "MRP Production Product Category Restriction",
+    "version": "16.0.1.0.1",
     "author": "Quartile Limited",
     "website": "https://www.quartile.co",
     "category": "MRP",
     "license": "AGPL-3",
-    "depends": ["mrp"],
+    "depends": ["mrp_subcontracting"],
     "data": [
         "views/product_category_views.xml",
     ],
--- mrp_production_product_category_restriction/readme/DESCRIPTION.md
+++ mrp_production_product_category_restriction/readme/DESCRIPTION.md
@@ -1,3 +1,5 @@
 This module prevents the manufacturing of products through Manufacturing
 Orders (MO) if the product's category has the 'product_ok' field
 disabled.
+
+Subcontracting MOs are free from the above restrictions.
