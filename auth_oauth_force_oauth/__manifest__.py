06620f8d420b05b731c5a1488a020bbfa8b7e39d
Aung Ko Ko Lin (Quartile) <45355704+AungKoKoLin1997@users.noreply.github.com>
2024-03-01 15:55:43 +0630

    [16.0][3489][ADD] auth_oauth_force_oauth (#71)
    * [ADD] auth_oauth_block_password_login

---
+++ auth_oauth_force_oauth/__manifest__.py
@@ -0,0 +1,15 @@
+# Copyright 2024 Quartile Limited (https://www.quartile.co)
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Auth OAuth Force OAuth",
+    "version": "16.0.1.0.0",
+    "author": "Quartile Limited",
+    "website": "https://www.quartile.co",
+    "category": "Tools",
+    "license": "AGPL-3",
+    "depends": ["auth_oauth", "auth_signup"],
+    "data": [
+        "views/res_company_views.xml",
+    ],
+    "installable": True,
+}
