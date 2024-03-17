8a3ca006afebe46eb88553b2be1cfd0c039d5a2d
Aung Ko Ko Lin (Quartile) <45355704+AungKoKoLin1997@users.noreply.github.com>
2024-03-17 14:19:16 +0700

    [ADD] purchase_deposit_analytic (#75)

---
+++ purchase_deposit_analytic/__manifest__.py
@@ -0,0 +1,13 @@
+# Copyright 2024 Quartile Limited
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "Purchase Deposit Analytic",
+    "version": "16.0.1.0.0",
+    "author": "Quartile Limited",
+    "website": "https://www.quartile.co",
+    "category": "Purchase Management",
+    "license": "AGPL-3",
+    "depends": ["purchase_deposit", "purchase_analytic"],
+    "installable": True,
+}
