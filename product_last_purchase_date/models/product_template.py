7c2bfb3703315f623a88ce6a64fdf8582693fc75
Aungkokolin1997 <lin@quartile.co>
2024-05-16 06:23:42 +0000

    adj

--- product_last_purchase_date/models/product_template.py
+++ product_last_purchase_date/models/product_template.py
@@ -9,7 +9,7 @@ class ProductTemplate(models.Model):
 
     man_last_purchase_date = fields.Date(
         "Last Purchase Date (Man.)",
-        compute="_compute_last_purchase_date",
+        compute="_compute_man_last_purchase_date",
         inverse="_inverse_man_last_purchase_date",
         store=True,
         copy=False,
@@ -26,9 +26,8 @@ class ProductTemplate(models.Model):
     @api.depends(
         "product_variant_ids",
         "product_variant_ids.man_last_purchase_date",
-        "product_variant_ids.last_purchase_date",
     )
-    def _compute_last_purchase_date(self):
+    def _compute_man_last_purchase_date(self):
         unique_variants = self.filtered(
             lambda template: len(template.product_variant_ids) == 1
         )
@@ -36,14 +35,8 @@ class ProductTemplate(models.Model):
             template.man_last_purchase_date = (
                 template.product_variant_ids.man_last_purchase_date
             )
-            last_purchase_date = template.product_variant_ids.last_purchase_date
-            man_last_purchase_date = template.man_last_purchase_date
-            if man_last_purchase_date and man_last_purchase_date > last_purchase_date:
-                last_purchase_date = man_last_purchase_date
-            template.last_purchase_date = last_purchase_date
         for template in self - unique_variants:
             template.man_last_purchase_date = False
-            template.last_purchase_date = False
 
     # This method is triggered upon save, therefore the UX of last_purchase_date update
     # of product.template is not as instant as that of product.product.
@@ -53,3 +46,23 @@ class ProductTemplate(models.Model):
                 template.product_variant_ids.man_last_purchase_date = (
                     template.man_last_purchase_date
                 )
+
+    @api.depends(
+        "man_last_purchase_date",
+        "product_variant_ids",
+        "product_variant_ids.last_purchase_date",
+    )
+    def _compute_last_purchase_date(self):
+        unique_variants = self.filtered(
+            lambda template: len(template.product_variant_ids) == 1
+        )
+        for template in unique_variants:
+            last_purchase_date = template.product_variant_ids.last_purchase_date
+            man_last_purchase_date = template.man_last_purchase_date
+            if man_last_purchase_date and (
+                not last_purchase_date or man_last_purchase_date > last_purchase_date
+            ):
+                last_purchase_date = man_last_purchase_date
+            template.last_purchase_date = last_purchase_date
+        for template in self - unique_variants:
+            template.last_purchase_date = False
