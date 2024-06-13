3bc5a2e33b7e51fd017f7d80f9a64d6948657553
Aung Ko Ko Lin (Quartile) <45355704+AungKoKoLin1997@users.noreply.github.com>
2024-06-13 10:15:34 +0700

    [3874][ADD] project_task_additional_attributes #59
    [3874][ADD] project_task_additional_attributes #59

---
+++ project_task_additional_attributes/__manifest__.py
@@ -0,0 +1,14 @@
+# Copyright 2023 Quartile Limited
+# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl).
+{
+    "name": "Project Task Additional Attributes",
+    "version": "16.0.1.0.0",
+    "category": "project",
+    "website": "https://www.quartile.co",
+    "author": "Quartile Limited",
+    "license": "LGPL-3",
+    "depends": ["project"],
+    "data": [
+        "views/project_task_views.xml",
+    ],
+}
