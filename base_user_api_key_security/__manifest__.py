2353cb22ac5361610bd305a2ab31bbea9c38c1e9
Aungkokolin1997 <lin@quartile.co>
2024-11-13 02:16:55 +0000

    [ADD] base_user_api_key_security

---
+++ base_user_api_key_security/__manifest__.py
@@ -0,0 +1,17 @@
+# Copyright 2024 Quartile
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+{
+    "name": "Base User Api Key Security",
+    "version": "16.0.1.0.0",
+    "category": "Users",
+    "author": "Quartile",
+    "website": "https://www.quartile.co",
+    "license": "AGPL-3",
+    "depends": [
+        "base",
+    ],
+    "data": [
+        "views/res_users_views.xml",
+    ],
+    "installable": True,
+}
---
+++ base_user_api_key_security/readme/DESCRIPTION.md
@@ -0,0 +1,2 @@
+This module restricts API key generation for internal users to all users
+except those in the 'Settings' group.
