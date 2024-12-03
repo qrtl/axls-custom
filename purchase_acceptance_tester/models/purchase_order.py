9da66b0dce3239005555f80ddf7470e2bb0f54d5
Yoshi Tashiro <tashiro@quartile.co>
2024-12-03 06:44:15 +0000

    adjust _compute_need_acceptance_test()

--- purchase_acceptance_tester/models/purchase_order.py
+++ purchase_acceptance_tester/models/purchase_order.py
@@ -10,3 +10,14 @@ class PurchaseOrder(models.Model):
     acceptance_tester_id = fields.Many2one(
         "res.partner", domain=[("is_acceptance_tester", "=", True)]
     )
+    need_acceptance_test = fields.Boolean(compute="_compute_need_acceptance_test")
+
+    def _compute_need_acceptance_test(self):
+        for order in self:
+            order.need_acceptance_test = False
+            if order.order_line.filtered(
+                lambda x: x.product_id
+                and x.product_type != "service"
+                and x.qty_received != x.product_qty
+            ):
+                order.need_acceptance_test = True
