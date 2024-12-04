494a12962daf2ca6f12a5fd09f8068daa94fb394
Yoshi Tashiro (Quartile) <tashiro@quartile.co>
2024-12-04 09:53:31 +0900

    [5006][ADD] purchase_acceptance_tester (#105)

---
+++ purchase_acceptance_tester/__manifest__.py
@@ -0,0 +1,17 @@
+# Copyright 2024 Quartile
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+{
+    "name": "Purchase Acceptance Tester",
+    "version": "16.0.1.0.0",
+    "author": "Quartile Limited",
+    "website": "https://www.quartile.co",
+    "category": "purchase",
+    "license": "AGPL-3",
+    "depends": ["purchase"],
+    "data": [
+        "views/purchase_order_views.xml",
+        "views/res_config_settings_views.xml",
+        "views/res_partner_views.xml",
+    ],
+    "installable": True,
+}
---
+++ purchase_acceptance_tester/readme/CONFIGURE.md
@@ -0,0 +1,6 @@
+Go to purchase settings and select 'Enable Acceptance Tester Check' to make the
+Acceptance Tester field of purchase orders required on certain conditions (i.e. the
+order contains a non-service product that has not been fully received).
+
+For contact records that are to be used as Acceptance Testers, select the 'Is Acceptance
+Tester' field in the contact form.
