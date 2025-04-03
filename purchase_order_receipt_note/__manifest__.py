9a3aa06ee5a499202696dc0b810725085d805f08
Aungkokolin1997 <lin@quartile.co>
2025-04-03 11:22:50 +0000

    update

--- purchase_order_receipt_note/__manifest__.py
+++ purchase_order_receipt_note/__manifest__.py
@@ -1,4 +1,4 @@
-# Copyright 2025 Quartile Limited
+# Copyright 2025 Quartile (https://www.quartile.co)
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 {
     "name": "Purhcase Order Receipt Report",
@@ -7,11 +7,9 @@
     "author": "Quartile",
     "website": "https://www.quartile.co",
     "license": "AGPL-3",
-    "depends": [
-        "purchase",
-    ],
+    "depends": ["purchase_stock"],
     "data": [
-        "reports/purchase_order_receipt_report.xml",
+        "reports/purchase_order_receipt_note.xml",
         "reports/purchase_order_receipt_templates.xml",
     ],
     "installable": True,
