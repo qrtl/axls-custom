fcc1a446433fa88e26dc44386945649d50998561
Aungkokolin1997 <lin@quartile.co>
2025-04-22 07:49:06 +0000

    [IMP] stock_lot_analytic: improve for receiving with existing lot

--- stock_lot_analytic/models/stock_move_line.py
+++ stock_lot_analytic/models/stock_move_line.py
@@ -13,3 +13,16 @@ class StockMoveLine(models.Model):
             "analytic_distribution"
         ] = self.move_id.purchase_line_id.analytic_distribution
         return res
+
+    def _action_done(self):
+        res = super()._action_done()
+        self = self.exists()
+        for ml in self:
+            if not ml.lot_id:
+                continue
+            purchase_line = ml.move_id.purchase_line_id
+            if purchase_line.analytic_distribution != ml.lot_id.analytic_distribution:
+                ml.lot_id.write(
+                    {"analytic_distribution": purchase_line.analytic_distribution}
+                )
+        return res
