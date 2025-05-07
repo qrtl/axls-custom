7f4097225d395ab4467ed31cc71f3547ae9e35f8
Aung Ko Ko Lin (Quartile) <45355704+AungKoKoLin1997@users.noreply.github.com>
2025-05-07 14:16:02 +0700

    [5266][IMP] stock_lot_analytic: improve for receiving with existing lot (#114)
    [5266][IMP] stock_lot_analytic: improve for receiving with existing lot (#114)

---
+++ stock_lot_analytic/models/stock_move.py
@@ -0,0 +1,20 @@
+# Copyright 2025 Quartile (https://www.quartile.co)
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+from odoo import models
+
+
+class StockMove(models.Model):
+    _inherit = "stock.move"
+
+    def _action_done(self, cancel_backorder=False):
+        res = super()._action_done(cancel_backorder=cancel_backorder)
+        for move in self:
+            if (
+                move.location_id.usage == "internal"
+                or move.location_dest_id.usage != "internal"
+                or not move.lot_ids
+            ):
+                continue
+            move.lot_ids.write({"analytic_distribution": move.analytic_distribution})
+        return res
