a2f26867ae767c603d1d5017b90018246b99fcc8
Aungkokolin1997 <lin@quartile.co>
2025-07-23 09:03:43 +0000

    adj

--- analytic_distribution_filter/models/analytic_account_line.py
+++ analytic_distribution_filter/models/analytic_account_line.py
@@ -1,7 +1,7 @@
 # Copyright 2025 Quartile (https://www.quartile.co)
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
-from odoo import fields, models
+from odoo import api, fields, models
 
 
 class AnalyticAccountLine(models.Model):
@@ -12,3 +12,72 @@ class AnalyticAccountLine(models.Model):
     account_ids = fields.Many2many(
         "account.analytic.account", string="Related Accounts", required=True
     )
+
+    @api.onchange("plan_id")
+    def onchange_plan_id(self):
+        self.account_ids = False
+
+    def _search_reciprocal(self, account_id, plan_id):
+        return self.search(
+            [
+                ("account_id", "=", account_id),
+                ("plan_id", "=", plan_id),
+            ],
+            limit=1,
+        )
+
+    def _update_reciprocal_relations(self, old_account_ids=None):
+        for line in self:
+            current_ids = set(line.account_ids.ids)
+            old_ids = set(old_account_ids or [])
+            added_ids = current_ids - old_ids
+            removed_ids = old_ids - current_ids
+            for account_id in added_ids:
+                reciprocal = self._search_reciprocal(
+                    account_id, line.account_id.plan_id.id
+                )
+                if reciprocal:
+                    if line.account_id.id not in reciprocal.account_ids.ids:
+                        reciprocal.account_ids = [(4, line.account_id.id)]
+                else:
+                    self.create(
+                        {
+                            "account_id": account_id,
+                            "plan_id": line.account_id.plan_id.id,
+                            "account_ids": [(6, 0, [line.account_id.id])],
+                        }
+                    )
+            for account_id in removed_ids:
+                reciprocal = self._search_reciprocal(
+                    account_id, line.account_id.plan_id.id
+                )
+                if reciprocal and line.account_id.id in reciprocal.account_ids.ids:
+                    reciprocal.account_ids = [(3, line.account_id.id)]
+                    if not reciprocal.account_ids:
+                        reciprocal.unlink()
+
+    @api.model
+    def create(self, vals):
+        res = super().create(vals)
+        for record in res:
+            record._update_reciprocal_relations()
+        return res
+
+    def write(self, vals):
+        for record in self:
+            old_ids = record.account_ids.ids
+            super().write(vals)
+            record._update_reciprocal_relations(old_account_ids=old_ids)
+        return True
+
+    def unlink(self):
+        for record in self:
+            for acc in record.account_ids:
+                reciprocal = self._search_reciprocal(
+                    acc.id, record.account_id.plan_id.id
+                )
+                if reciprocal and record.account_id.id in reciprocal.account_ids.ids:
+                    reciprocal.account_ids = [(3, record.account_id.id)]
+                    if not reciprocal.account_ids:
+                        reciprocal.unlink()
+        return super().unlink()
