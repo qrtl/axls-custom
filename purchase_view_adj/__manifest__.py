4c9df6fda1b601361f5521806898afb6199c700b
Yoshi Tashiro (Quartile) <tashiro@quartile.co>
2025-10-28 15:00:59 +0900

    [5860][IMP] purchase_view_adj: selection widget to picking_type_id (#125)
    [5860][IMP] purchase_view_adj: selection widget to picking_type_id (#125)

--- purchase_view_adj/__manifest__.py
+++ purchase_view_adj/__manifest__.py
@@ -2,12 +2,12 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 {
     "name": "Purchase View Adj",
-    "version": "16.0.1.0.0",
+    "version": "16.0.1.1.0",
     "author": "Quartile Limited",
     "website": "https://www.quartile.co",
     "category": "Purchase",
     "license": "AGPL-3",
-    "depends": ["purchase"],
+    "depends": ["purchase_stock"],
     "data": [
         "views/purchase_order_views.xml",
     ],
