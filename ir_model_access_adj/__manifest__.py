1d1e058e96033d8bec1406d3c91681d451baadeb
nobuQuartile <nobu@quartile.co>
2025-10-30 01:50:13 +0000

    [5799][ADD] ir_model_access_adj

---
+++ ir_model_access_adj/__manifest__.py
@@ -0,0 +1,15 @@
+# Copyright 2025 Quartile Limited (https://www.quartile.co)
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Model Access Adjust",
+    "version": "16.0.1.0.0",
+    "author": "Quartile Limited",
+    "website": "https://www.quartile.co",
+    "category": "Users",
+    "license": "AGPL-3",
+    "depends": ["base", "purchase"],
+    "data": [
+        "data/ir_model_access_data.xml",
+    ],
+    "installable": True,
+}
---
+++ ir_model_access_adj/readme/DESCRIPTION.md
@@ -0,0 +1,2 @@
+This module is designed to manage and adjust model access permissions.
+All permission data is maintained in an XML file, where each permission setting can be explained through comments.
