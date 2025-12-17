5c2747a3918404cdc7635d711fb4080760c31ee9
nobuQuartile <nobu@quartile.co>
2025-12-17 06:00:15 +0000

    upd Customer Return > Component Return

--- inventory_reporting/reports/inventory_report.py
+++ inventory_reporting/reports/inventory_report.py
@@ -151,8 +151,23 @@ class InventoryReportXlsx(models.AbstractModel):
                 ],
             },
             {
-                "name": _("Customer Return"),
+                "name": _("Component Return"),
                 "filter": [
+                    "|",
+                    "&",
+                    ("stock_move_id.location_dest_id.usage", "=", "production"),
+                    (
+                        "stock_move_id.location_id.is_subcontracting_location",
+                        "=",
+                        False,
+                    ),
+                    "&",
+                    ("stock_move_id.location_id.usage", "=", "production"),
+                    (
+                        "stock_move_id.location_dest_id.is_subcontracting_location",
+                        "=",
+                        False,
+                    ),
                     ("stock_move_id.picking_code", "=", "incoming"),
                     ("stock_move_id.origin_returned_move_id", "!=", False),
                 ],
