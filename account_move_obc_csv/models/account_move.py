6240444f4e1aa49b5aa410a64a1ced19c378ef7a
Tatsuki Kanda (Quartile) <101688144+kanda999@users.noreply.github.com>
2026-02-28 11:08:24 +0900

    [6120][IMP] account_move_obc_csv (#139)
    [6120][IMP] account_move_obc_csv (#139)

--- account_move_obc_csv/models/account_move.py
+++ account_move_obc_csv/models/account_move.py
@@ -1,10 +1,25 @@
 # Copyright 2023 Quartile Limited
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
-from odoo import fields, models
+from odoo import _, fields, models
 
 
 class AccountMove(models.Model):
     _inherit = "account.move"
 
-    is_exported = fields.Boolean("Exported", copy=False)
+    is_exported = fields.Boolean("Exported", copy=False, tracking=True)
+
+    def action_unset_exported(self):
+        """Unset is_exported field for selected records"""
+        count = len(self)
+        self.write({"is_exported": False})
+        message = _("%(count)s record(s) unmarked as exported.") % {"count": count}
+        return {
+            "type": "ir.actions.client",
+            "tag": "display_notification",
+            "params": {
+                "type": "success",
+                "message": message,
+                "sticky": False,
+            },
+        }
